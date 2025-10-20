91213bdc50518d071214aa2b3fd4644388bdccc1
takumi1991 <78605227+takumi1991@users.noreply.github.com>
2025-10-21 02:22:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "✅ horse-nft-lab-clean is alive!"
+
+if __name__ == "__main__":
+    import os
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 8080)))
