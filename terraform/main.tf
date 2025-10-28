03f2d91a1b0d1c8edcdc27d505611f9524648cf8
takumi1991 <78605227+takumi1991@users.noreply.github.com>
2025-10-29 01:51:07 +0900

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,3 +12,25 @@ resource "google_monitoring_slo" "availability_99" {
     }
   }
 }
+
+
+resource "google_monitoring_alert_policy" "slo_burnrate_alert" {
+  display_name = "SLO Burn Rate Alert"
+  combiner     = "OR"
+
+  conditions {
+    display_name = "Burn rate above 10"
+    condition_monitoring_query_language {
+      query = <<EOT
+fetch slo("projects/horse-nft-lab-clean/services/horse-nft-lab-clean/serviceLevelObjectives/${google_monitoring_slo.availability_99.id}")
+| condition val() > 10
+EOT
+    }
+  }
+
+  notification_channels = [
+    "projects/horse-nft-lab-clean/notificationChannels/YOUR_SLACK_CHANNEL_ID"
+  ]
+
+  enabled = true
+}
