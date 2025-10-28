0156046e74df19c0aa21506fea473842b50d8877
takumi1991 <78605227+takumi1991@users.noreply.github.com>
2025-10-29 01:49:30 +0900

    Create versions.tf

---
+++ terraform/versions.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+
+  cloud {
+    organization = "YOUR_TERRAFORM_CLOUD_ORG"  # Terraform Cloud上のOrganization名
+    workspaces {
+      name = "horse-nft-lab-clean"
+    }
+  }
+}
+
+provider "google" {
+  project = "horse-nft-lab-clean"
+  region  = "asia-northeast1"
+}
