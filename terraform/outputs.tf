8c5ac5faf41632401f8fcf784440bddc9c848b59
takumi1991 <78605227+takumi1991@users.noreply.github.com>
2025-10-29 01:02:48 +0900

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,9 @@
+output "monitoring_service_name" {
+  value       = google_monitoring_service.run.name
+  description = "Full name of Monitoring Service (projects/.../services/...)"
+}
 
+output "slo_name" {
+  value       = google_monitoring_slo.availability_99.name
+  description = "Full name of SLO (projects/.../services/.../serviceLevelObjectives/...)"
+}
