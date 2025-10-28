e1a524c5b65fa79f4c9bab98df728147cb9aab48
takumi1991 <78605227+takumi1991@users.noreply.github.com>
2025-10-29 02:33:56 +0900

    Update versions.tf

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,12 +1,13 @@
 terraform {
-  required_version = ">= 1.6.0"
+  required_version = ">= 1.5.0"
 
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 5.0"
+      version = "~> 5.30.0"
     }
   }
+}
 
   cloud {
     organization = "YOUR_TERRAFORM_CLOUD_ORG"  # Terraform Cloud上のOrganization名
